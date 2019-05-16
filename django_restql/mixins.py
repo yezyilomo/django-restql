ea3d8fb480ad888c88835192f0c6363d009ad715
yezy <yezileliilomo@hotmail.com>
2019-05-17 00:07:49 +0300

    :bug:Fix breaking DRF permissions
    -Availability of all resources and their fields are now controlled by the DRF it self
    -Filtering is now done on the output from the serializer instead of the model instance

--- django_restql/mixins.py
+++ django_restql/mixins.py
@@ -1,56 +1,44 @@
 import json
-import datetime
-import warnings
 
 import dictfier
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
 
 class DynamicFieldsMixin():
     query_param_name = "query"
 
-    def flat_field(self, obj, parent_obj):
-        if isinstance(obj, (str, int, bool, float)):
-            return obj
-        if isinstance(obj, datetime.datetime):
-            return obj.strftime("%Y-%m-%d-%H-%M")
-        if isinstance(obj, datetime.date):
-            return obj.strftime("%Y-%m-%d")
-        if isinstance(obj, datetime.time):
-            return obj.strftime("%H-%M-%S")
-        cls_name = obj.__class__.__name__
-        if cls_name in ("ManyRelatedManager", "RelatedManager"):
-            return [sub_obj.pk for sub_obj in obj.all()]
-        if hasattr(obj, "pk"):
-            return obj.pk
-        raise ValueError(
-            "django-restql failed to serialize an object of type '%s'" 
-            % cls_name
+    def list(self, request):
+        queryset = self.get_queryset()
+        filtered_queryset = self.filter_queryset(queryset)
+        paginated_queryset = self.paginate_queryset(filtered_queryset)
+        serializer = self.get_serializer(
+            paginated_queryset, many=True, context={'request': request}
         )
-
-    def nested_flat_field(self, obj, parent_obj):
-        return obj
-
-    def nested_iter_field(self, obj, parent_obj):
-        cls_name = obj.__class__.__name__
-        if cls_name in ("ManyRelatedManager", "RelatedManager"):
-            return obj.all()
-        return obj
-
-    def to_representation(self, instance):
-        try:
-            request = self.context['request']
-        except KeyError:
-            warnings.warn('Context does not have access to request.')
-            return super().to_representation(instance)
 
         if self.query_param_name in request.query_params:
             query = json.loads(request.query_params[self.query_param_name])
-            data = dictfier.dictfy(
-                instance, 
-                query,
-                flat_obj=self.flat_field,
-                nested_flat_obj=self.nested_flat_field,
-                nested_iter_obj=self.nested_iter_field,
+
+            data = dictfier.filter(
+                serializer.data,
+                query
             )
-            return data
-            
-        return super().to_representation(instance)
+            return self.get_paginated_response(data)
+        return self.get_paginated_response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = self.get_queryset()
+        object = get_object_or_404(queryset, pk=pk)
+        serializer = self.get_serializer(
+            object, context={'request': request}
+        )
+
+        if self.query_param_name in request.query_params:
+            query = json.loads(request.query_params[self.query_param_name])
+
+            data = dictfier.filter(
+                serializer.data,
+                query
+            )
+            return Response(data)
+        return Response(serializer.data)
