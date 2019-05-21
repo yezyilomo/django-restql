91e831f9df1ab1a5e9ecb20b5d85e3778001116c
yezy <yezileliilomo@hotmail.com>
2019-05-22 01:51:51 +0300

    :sparkles:Remove nested list on query parameter
    - fixes #15

--- django_restql/mixins.py
+++ django_restql/mixins.py
@@ -4,11 +4,25 @@ import dictfier
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
+def format_query(query, schema):
+    for field in query:
+        if isinstance(field, dict):
+            for nested_field in field:
+                if isinstance(schema[nested_field], list):
+                    # Iterable nested field
+                    field[nested_field] = [field[nested_field]]
+                    format_query(schema[nested_field], field[nested_field][0])
+                else:
+                    # Flat nested field
+                    format_query(schema[nested_field], field[nested_field])
+        else:
+            pass
 
 class DynamicFieldsMixin():
     query_param_name = "query"
 
     def list(self, request):
+        schema = self.get_serializer().data
         queryset = self.get_queryset()
         if self.filter_backends is not None:
             queryset = self.filter_queryset(queryset)
@@ -27,6 +41,9 @@ class DynamicFieldsMixin():
 
         if self.query_param_name in request.query_params:
             query = json.loads(request.query_params[self.query_param_name])
+
+            format_query(query, schema)
+            query = [query]
             data = dictfier.filter(
                 serializer.data,
                 query
@@ -36,6 +53,7 @@ class DynamicFieldsMixin():
         return response(serializer.data)
 
     def retrieve(self, request, pk=None):
+        schema = self.get_serializer().data
         queryset = self.get_queryset()
         object = get_object_or_404(queryset, pk=pk)
         serializer = self.get_serializer(
@@ -45,6 +63,7 @@ class DynamicFieldsMixin():
         if self.query_param_name in request.query_params:
             query = json.loads(request.query_params[self.query_param_name])
 
+            format_query(query, schema)
             data = dictfier.filter(
                 serializer.data,
                 query
