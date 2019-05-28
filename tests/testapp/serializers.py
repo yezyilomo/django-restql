244d4084a308aa2db28eddfba6ee48468e023118
yezy <yezileliilomo@hotmail.com>
2019-05-28 11:22:03 +0300

    :white_check_mark:Add test for reverse relation

--- tests/testapp/serializers.py
+++ tests/testapp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from tests.testapp.models import Book, Course, Student
+from tests.testapp.models import Book, Course, Student, Phone
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,15 +8,20 @@ class BookSerializer(serializers.ModelSerializer):
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    books = BookSerializer(many=True, read_only=False)
+    books = BookSerializer(many=True, read_only=True)
     class Meta:
         model = Course
         fields = ['name', 'code', 'books']
 
 
+class PhoneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Phone
+        fields = ['number', 'type']
+
 class StudentSerializer(serializers.ModelSerializer):
-    course = CourseSerializer(many=False, read_only=False)
+    course = CourseSerializer(many=False, read_only=True)
+    phone_numbers = PhoneSerializer(many=True, read_only=True)
     class Meta:
         model = Student
-        fields = ['name', 'age', 'course']
-
+        fields = ['name', 'age', 'course', 'phone_numbers']
