0107191a4bf7952bc61728eb59ea4179b527e0ff
yezy <yezileliilomo@hotmail.com>
2019-05-28 11:32:21 +0300

    :bug:Fix skipping sub fields of a nested field on reverse relations

--- django_restql/mixins.py
+++ django_restql/mixins.py
@@ -2,6 +2,7 @@ import re
 import json
 
 import dictfier
+from rest_framework import serializers
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
@@ -11,7 +12,7 @@ def get_formatted_query(raw_query, schema):
     for field in raw_query:
         if isinstance(field, dict):
             for nested_field in field:
-                if isinstance(schema[nested_field], list):
+                if isinstance(schema[nested_field], serializers.ListSerializer):
                     # Iterable nested field
                     nested_iterable_node = get_formatted_query(
                         field[nested_field],
@@ -59,7 +60,7 @@ class DynamicFieldsMixin():
     query_param_name = "query"
 
     def list(self, request):
-        schema = self.get_serializer().data
+        schema = self.get_serializer().get_fields()
         queryset = self.get_queryset()
         if self.filter_backends is not None:
             queryset = self.filter_queryset(queryset)
@@ -79,19 +80,17 @@ class DynamicFieldsMixin():
         if self.query_param_name in request.query_params:
             query_str = request.query_params[self.query_param_name]
             raw_query = parse_query(query_str)
-
             query = get_formatted_query(raw_query, schema)
             query = [query]  # extra [] because a list is returned in this case
             data = dictfier.filter(
                 serializer.data,
                 query
             )
-
             return response(data)
         return response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        schema = self.get_serializer().data
+        schema = self.get_serializer().get_fields()
         queryset = self.get_queryset()
         object = get_object_or_404(queryset, pk=pk)
         serializer = self.get_serializer(
