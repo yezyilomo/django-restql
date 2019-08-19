09df33b7864976c05041b856fe05c58d16eeec7a
yezy <yezileliilomo@hotmail.com>
2019-08-19 18:01:14 +0300

    :white_check_mark:Update tests

--- tests/testapp/serializers.py
+++ tests/testapp/serializers.py
@@ -1,25 +1,26 @@
 from rest_framework import serializers
+from django_restql.mixins import DynamicFieldsMixin
 from tests.testapp.models import Book, Course, Student, Phone
 
-class BookSerializer(serializers.ModelSerializer):
+class BookSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['title', 'author']
 
 
-class CourseSerializer(serializers.ModelSerializer):
+class CourseSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     books = BookSerializer(many=True, read_only=True)
     class Meta:
         model = Course
         fields = ['name', 'code', 'books']
 
 
-class PhoneSerializer(serializers.ModelSerializer):
+class PhoneSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = Phone
         fields = ['number', 'type']
 
-class StudentSerializer(serializers.ModelSerializer):
+class StudentSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     course = CourseSerializer(many=False, read_only=True)
     phone_numbers = PhoneSerializer(many=True, read_only=True)
     class Meta:
--- tests/testapp/views.py
+++ tests/testapp/views.py
@@ -5,16 +5,16 @@ from django_restql.mixins import DynamicFieldsMixin
 from tests.testapp.models import Book, Course, Student
 from tests.testapp.serializers import BookSerializer, CourseSerializer, StudentSerializer
 
-class BookViewSet(DynamicFieldsMixin, viewsets.ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
 	serializer_class = BookSerializer
 	queryset = Book.objects.all()
 
 
-class CourseViewSet(DynamicFieldsMixin, viewsets.ModelViewSet):
+class CourseViewSet(viewsets.ModelViewSet):
 	serializer_class = CourseSerializer
 	queryset = Course.objects.all()
 
 
-class StudentViewSet(DynamicFieldsMixin, viewsets.ModelViewSet):
+class StudentViewSet(viewsets.ModelViewSet):
 	serializer_class = StudentSerializer
 	queryset = Student.objects.all()
