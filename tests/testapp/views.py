cae07eb0b40b63088da74b9e7e462b7d34fc0be5
yezy <yezileliilomo@hotmail.com>
2019-09-06 13:51:04 +0300

    :white_check_mark:Update tests

--- tests/testapp/views.py
+++ tests/testapp/views.py
@@ -4,7 +4,9 @@ from django_restql.mixins import DynamicFieldsMixin
 
 from tests.testapp.models import Book, Course, Student
 from tests.testapp.serializers import (
-	BookSerializer, CourseSerializer, StudentSerializer
+	BookSerializer, CourseSerializer, StudentSerializer,
+	CourseWithFieldsKwargSerializer, CourseWithExcludeKwargSerializer,
+	CourseWithReturnPkkwargSerializer
 )
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -17,6 +19,17 @@ class CourseViewSet(viewsets.ModelViewSet):
 	serializer_class = CourseSerializer
 	queryset = Course.objects.all()
 
+class CourseWithReturnPkkwargViewSet(viewsets.ModelViewSet):
+	serializer_class = CourseWithReturnPkkwargSerializer
+	queryset = Course.objects.all()
+
+class CourseWithFieldsKwargViewSet(viewsets.ModelViewSet):
+	serializer_class = CourseWithFieldsKwargSerializer
+	queryset = Course.objects.all()
+
+class CourseWithExcludeKwargViewSet(viewsets.ModelViewSet):
+	serializer_class = CourseWithExcludeKwargSerializer
+	queryset = Course.objects.all()
 
 class StudentViewSet(viewsets.ModelViewSet):
 	serializer_class = StudentSerializer
