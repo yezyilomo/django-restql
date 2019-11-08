8dc522a951e9764eda9ec034119045cf02b7ffcb
yezy <yezileliilomo@hotmail.com>
2019-11-08 19:48:29 +0300

    :art:Improve format of the code

--- django_restql/exceptions.py
+++ django_restql/exceptions.py
@@ -1,10 +1,10 @@
-
 class DjangoRESTQLException(Exception):
     """Base class for exceptions in this module."""
 
 
 class InvalidOperation(DjangoRESTQLException):
     """Invalid Operation Exception."""
+
 
 class FieldNotFound(DjangoRESTQLException):
     """Field Not Found Exception."""
