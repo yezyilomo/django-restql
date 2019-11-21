97afd11be1dc69eae251f3389e9824ab9507da79
yezy <yezileliilomo@hotmail.com>
2019-11-22 00:21:17 +0300

    :art:Break line exceeding 80 chars

--- django_restql/fields.py
+++ django_restql/fields.py
@@ -17,7 +17,8 @@ UPDATE_SUPPORTED_OPERATIONS = (ADD, CREATE, REMOVE, UPDATE)
 class DynamicSerializerMethodField(SerializerMethodField):
     def to_representation(self, value):
         method = getattr(self.parent, self.method_name)
-        if hasattr(self.parent, "nested_fields") and self.field_name in self.parent.nested_fields:
+        if (hasattr(self.parent, "nested_fields") and 
+                self.field_name in self.parent.nested_fields):
             query = self.parent.nested_fields[self.field_name]
         else:
             query = {
