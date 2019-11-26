61071647a557437a0c2925f132d3ba80818d1ba9
yezy <yezileliilomo@hotmail.com>
2019-11-26 22:06:28 +0300

    :bookmark:Release v0.7.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ REQUIRES_PYTHON = '>=3.5'
 
 setup(
     name = 'django-restql',
-    version = '0.7.0',
+    version = '0.7.1',
     description = DESCRIPTION,
     long_description = readme,
     long_description_content_type = 'text/markdown',
