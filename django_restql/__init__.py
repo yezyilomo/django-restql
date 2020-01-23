30788746c23d3496585dd5f8b6416707af2326e7
yezy <yezileliilomo@hotmail.com>
2020-01-23 22:08:11 +0300

    :bookmark:Release v0.8.1

--- django_restql/__init__.py
+++ django_restql/__init__.py
@@ -1,7 +1,7 @@
 __title__ = 'Django RESTQL'
 __description__ = 'Turn your API made with Django REST Framework(DRF) into a GraphQL like API.'
 __url__ = 'https://github.com/yezyilomo/django-restql'
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 __author__ = 'Yezy Ilomo'
 __author_email__ = 'yezileliilomo@hotmail.com'
 __license__ = 'MIT'
